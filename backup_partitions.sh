ace05873a627b83d3ceaf406968abea6417eb312
Constantin Gavrilescu <costi@costi-personal.(none)>
2010-07-14 20:39:03 -0500

    Fixed syntax errors

---
+++ backup_partitions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script backs up the partition information and the MBR
+
+DEVICE="$1"
+
+if [ -z $DEVICE ]; then
+    echo "Usage: $(basename $0) <device>"
+    echo "Example: $(basename $0) /dev/sda"
+    exit 1
+fi
+
+BACKUP_DIR='/backups'
+
+fdisk -l $DEVICE > $BACKUP_DIR/fdisk.txt
+dd if=$DEVICE of=$BACKUP_DIR/mbr bs=512 count=1
